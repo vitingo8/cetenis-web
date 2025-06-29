e719eab6ebae043b95031c3737e3739fb8798ba7
David <davispuny@gmail.com>
2025-06-29 13:47:08 +0200

    Web

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: '.',
-  base: './',
-  server: {
-    port: 3000,
-    open: true,
-    host: true
-  },
-  build: {
-    outDir: 'dist',
-    minify: 'esbuild',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+  base: './'
 }) 
